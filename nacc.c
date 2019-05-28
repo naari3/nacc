32cf5081691e673fe99e51725d5f459727a57112
naari3 <naari.named@gmail.com>
2019-05-28 09:51:34 +0900

    Split Vector

--- nacc.c
+++ nacc.c
@@ -1,45 +1,5 @@
 #include "nacc.h"
 
-Vector *new_vector() {
-  Vector *vec = malloc(sizeof(Vector));
-  vec->data = malloc(sizeof(void *) * 16);
-  vec->capacity = 16;
-  vec->len = 0;
-  return vec;
-}
-
-void vec_push(Vector *vec, void *elem) {
-  if (vec->capacity == vec->len) {
-    vec->capacity *= 2;
-    vec->data = realloc(vec->data, sizeof(void *) * vec->capacity);
-  }
-  vec->data[vec->len++] = elem;
-}
-
-void expect(int line, int expected, int actual) {
-  if (expected == actual) return;
-  fprintf(stderr, "%d: %d expected, but got %d\n", line, expected, actual);
-  exit(1);
-}
-
-void runtest() {
-  Vector *vec = new_vector();
-  expect(__LINE__, 0, vec->len);
-
-  // intだとサイズが違って警告が出るので代わりにlongを使う
-  // warning: cast to pointer from integer of different size
-  // [-Wint-to-pointer-cast]
-  // for (int i = 0; i < 100; i++) vec_push(vec, (void *)i);
-  for (long i = 0; i < 100; i++) vec_push(vec, (void *)i);
-
-  expect(__LINE__, 100, vec->len);
-  expect(__LINE__, 0, (long)vec->data[0]);
-  expect(__LINE__, 50, (long)vec->data[50]);
-  expect(__LINE__, 99, (long)vec->data[99]);
-
-  printf("OK\n");
-}
-
 int pos = 0;
 char *user_input;
 
